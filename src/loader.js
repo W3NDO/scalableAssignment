e1053ddcd17f43ef725bbe21da9e954af1a236ce
W3NDO <wendonyang@gmail.com>
2023-12-11 19:37:00 +0100

    feat(main) -new dataset with more fields.

--- src/loader.js
+++ src/loader.js
@@ -11,35 +11,18 @@ const { finished } = require('stream/promises')
 
 const { Graph, GraphEdgeCollection, GraphVertexCollection } = require('arangojs/graph');
 
+/*** 
+ * Arango Client Setup 
+ */
 const arangoClient = arangojs({
     url: "http://localhost:8529",
     databaseName: "TestArango",
     auth: { username: "root", password: "pswd1" },
   });
 
-// const collection = db.collection("anime-ontology");
-// const graph = db.graph("testEdgeCollection");
-// console.log(graph)
-// const collection = graph.edgeCollection("testEdges")
-// try {
-//   const edge = collection.edge("abc123");
-//   console.log(edge);
-// } catch (e) {
-//   console.error("Could not find edge");
-// }
-
-// arangoClient.listCollections()
-//   .then(function (collections) {
-//     console.log("Your Arango collections: " + collections.map(function (collection) {
-//         return collection.name;
-//     }).join(", "));
-//   });
-
 /***
  * MongoClient Setup
 */
-
-
 const mongoUri = "mongodb://localhost:27888"
 const mongoClient = new MongoClient(mongoUri, {
   serverApi: {
@@ -63,12 +46,9 @@ async function pingMongo(){
 pingMongo().catch(console.dir);
 
 
-/***
- * Other stuff
- */
+// LOAD ARANGO GRAPH
 
-// LOAD GRAPH
-
+// reads in a csv file and return an interable array of objects
 async function loadDataAsObjects(filePath) {
   const records = [];
   const parser = fs
@@ -86,33 +66,35 @@ async function loadDataAsObjects(filePath) {
   return records;
 };
 
+// inserts documents into a collection
 async function arangoLoadNodes(client, filePath){
   var nodes = await loadDataAsObjects(filePath)
-  let [n, iterNodes] = nodes
   for (const node of nodes){
+    var [name, age, username] = node
     var nodeInsertQuery;
     if(true){
       nodeInsertQuery = aql`
         UPSERT {
-          _key: ${node[0]},
-          name: ${node[0]}
+          _key: ${username},
+          name: ${name},
+          age: ${age}
         }
         INSERT {
-          _key: ${node[0]},
-          name: ${node[0]}
+          _key: ${username},
+          name: ${name},
+          age: ${age}
         }
         UPDATE {}
-        IN "fake_social_media"
+        IN "fakeSocialMediaWithAge"
       `
     }
-      // console.log("QUERY: ", nodeInsertQuery)
     await client.query(nodeInsertQuery)
   }
 }
 
-async function arangoLoadEdges(client, filePath){
+// inserts edge definition documents into an edge collection
+async function arangoLoadEdges(client, filePath, nodeCollectionName, edgeCollectionName){
   var records = await loadDataAsObjects(filePath)
-  let nodes;
   for(const record of records ){
     const [edge, srcNode, destNode] = record
     if (srcNode === "subject") { continue }
@@ -120,72 +102,16 @@ async function arangoLoadEdges(client, filePath){
     var edgeInsertQuery = {
       "query": "UPSERT { _from: @sourceNode, _to: @destNode, edge: {name: @edgeName} } INSERT { _from: @sourceNode, _to: @destNode, edge: {name: @edgeName }} UPDATE {} IN @@edgeCollection ",
       "bindVars" : {
-          "@edgeCollection": "fake_social_media_edges",
-          "sourceNode": `fake_social_media/${srcNode}` ,
-          "destNode": `fake_social_media/${destNode}`,
+          "@edgeCollection": edgeCollectionName,
+          "sourceNode": `${nodeCollectionName}/${srcNode}` ,
+          "destNode": `${nodeCollectionName}/${destNode}`,
           "edgeName": "follows",
       }
     }
 
-    await client.query(edgeInsertQuery);
-    // let arangoDestNodeID = await client.query(destNodeInsertQuery);
-    
+    await client.query(edgeInsertQuery);   
   }  
-
-  const getSrcDestNodes = async () => {
-    let collectionName = "fake_social_media"
-    var getNodesQuery = {
-      "query": "FOR u IN @@collection RETURN u",
-      "bindVars": {
-        "@collection": "fake_social_media"
-      }   
-    }
-    nodes = await client.query(getNodesQuery);
-    let edgeSrcDest = [];
-
-    await nodes.forEach(async n => {
-      if (n.follows){
-        edgeSrcDest.push({
-          from : `${collectionName}/${n._key}` || null, 
-          edge : "follows" ,
-          to : `${collectionName}/${n.follows}` || null
-        })
-      }
-    });
-    return edgeSrcDest;
-  }
-
-  const buildEdges = async () => {
-    let buildEdgesQuery = async (edgeBuilderObject) => {
-      query =  {
-        "query": "UPSERT { _from: @sourceNode, _to: @destNode, edge: { name: @edgeName } } INSERT { _from: @sourceNode, _to: @destNode, edge: { name: @edgeName } } UPDATE {} INTO @@edgeCollection ",
-        "bindVars" : {
-            "@edgeCollection": "fake_social_media_edges",
-            "sourceNode": edgeBuilderObject["from"] ,
-            "destNode": edgeBuilderObject["to"],
-            "edgeName": edgeBuilderObject["edge"],
-        }
-      }
-
-      await client.query(query)
-    }
-
-    const edgeBuilderInfo = await getSrcDestNodes();
-    let queryList = []
-    edgeBuilderInfo.forEach((user) => {
-      queryList.push(buildEdgesQuery(user))
-    })
-    return queryList;
-  }
-
-  // console.log( "QUERY : ", await buildEdges())
-  let queries = await buildEdges()
-  // queries.forEach( async query => {
-  //   await client.query(query)
-  // })
 }
-
-
 
 let serverStatus = [arangoClient, mongoClient]
 
@@ -196,23 +122,20 @@ const timeQuery = (query, databaseType) => {
 }
 
 console.log("MongoDB vs ArangoDB: The grand showdown!")
-// console.log(serverStatus)
 
-// console.time("Arango Load Time")
-// arangoLoadNodes(arangoClient, '/home/w3ndo/Desktop/Course Work/Scalable Data Management Systems/project/src/datasets/fake_users.csv' )
-//   .then( ()=> {
-//     console.log('Graph Data Loaded successfully')
-//   })
-//   .catch( (err) => [
-//     console.log('Error Loading Graph Data: ', err )
-//   ])
-  arangoLoadNodes(arangoClient, '/home/w3ndo/Desktop/Course Work/Scalable Data Management Systems/project/src/datasets/fake_users.csv' )
-  arangoLoadEdges(arangoClient, '/home/w3ndo/Desktop/Course Work/Scalable Data Management Systems/project/src/datasets/fake_social_media.csv' )
+// build the graph on arangoDB
+const buildArangoGraph = async (nodeFilePath, edgesFilePath ) => {
+  arangoLoadNodes(arangoClient, nodeFilePath )
+  arangoLoadEdges(arangoClient, edgesFilePath, "fakeSocialMediaWithAge" ,"fakeSocialMediaWithAgeEdges")
   .then( ()=> {
-    console.log('Graph Data Loaded successfully')
+    console.log('ARANGO :: Graph Data Loaded successfully')
   })
   .catch( (err) => [
-    console.log('Error Loading Graph Data: ', err )
+    console.log('ARANGO :: Error Loading Graph Data: ', err )
   ])
-// console.timeEnd("Arango Load Time")
-// console.timeLog("Arango Load Time")
+}
+
+nodeFilePath = '/home/w3ndo/Desktop/Course Work/Scalable Data Management Systems/project/src/datasets/fake_users2.csv'
+edgeDefinitionFilePath = '/home/w3ndo/Desktop/Course Work/Scalable Data Management Systems/project/src/datasets/fake_social_media2.csv'
+
+buildArangoGraph( nodeFilePath , edgeDefinitionFilePath )
--- src/scripts/faker.rb
+++ src/scripts/faker.rb
@@ -4,13 +4,24 @@ require 'csv'
 def build_triple # build triples in batches of 10. At least 20% follow each other.
   triples = Array.new
 
-  users = (1..10).map { Faker::Name.first_name }
+  # users = (1..10).map { 
+  #   {:key => :name => Faker::Name.first_name, :age => rand(12..100)} 
+  # }
+  users = []
+  10.times do 
+    name = Faker::Name.first_name
+    age = rand(12..100)
+    key = "#{name}_#{age}"
+    users << { :key => key, :name => name, :age => age }
+  end
+
   users.each do |user|
-    triples << ["follows", user, (users - [user]).sample].join(",")
+    followed = (users - [user]).sample
+    triples << ["follows", "#{user[:key]}", "#{followed[:key]}"].join(",")
   end
   popular_user = users.sample
   3.times do 
-    triples << ["follows", popular_user, (users - [popular_user]).sample].join(",")
+    triples << [ "follows", popular_user[:key], ((users - [popular_user]).sample)[:key] ].join(",")
   end
 
   [triples, users]
@@ -18,23 +29,24 @@ end
 
 triples = []
 users = []
-30000.times do 
+3.times do 
   new_triple = build_triple
   triples << new_triple[0]
   users << new_triple[1]
 end
 
-CSV.open('./datasets/fake_social_media.csv', 'w') do |file|
+pp users
+pp triples
+
+CSV.open('./datasets/fake_social_media2.csv', 'w') do |file|
   file << ['predicate', 'subject', 'object']
   triples.flatten.each do |triple|
     file << triple.split(",")
   end
 end
 
-CSV.open('./datasets/fake_users.csv', 'w') do |csv|
-  csv << ['Name']
+CSV.open('./datasets/fake_users2.csv', 'w') do |csv|
+  csv << ['Name','Age','Username']
 
-  users.flatten.each { |name| csv << [name] }
+  users.flatten.each { |user| csv << [user[:name],user[:age],user[:key]] }
 end
-
-pp users.flatten
