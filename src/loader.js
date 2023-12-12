987fa591f08beab1b6a3dc87949380d720c198df
W3NDO <wendonyang@gmail.com>
2023-12-12 12:32:11 +0100

    feat(main) - singleRead and Single Write queries added for Arango - made the file paths for nodeFilePath and EdgesFilePath system agnostic

--- src/loader.js
+++ src/loader.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const arangojs = require("arangojs");
 const { aql } = require("arangojs/aql");
 const fs = require("fs");
+var path = require('path');
 const { parse } = require("csv-parse");
 const { finished } = require("stream/promises");
 
@@ -27,8 +28,9 @@ const arangoClient = arangojs({
 /***
  * MongoClient Setup
  */
-const mongoUri =
-  "mongodb+srv://bogdandragomirgeorge:bEfNfE1qoLmiNH4y@cluster0.bioh4ut.mongodb.net/?retryWrites=true&w=majority";
+const mongoUri = "mongodb://localhost:27888"
+// const mongoUri =
+//   "mongodb+srv://bogdandragomirgeorge:bEfNfE1qoLmiNH4y@cluster0.bioh4ut.mongodb.net/?retryWrites=true&w=majority";
 const mongoClient = new MongoClient(mongoUri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -56,9 +58,9 @@ const mongoClient = new MongoClient(mongoUri, {
 // aggregation query 100 times
 // find everyone who follows who follows this user (find all mutual friends)
 
-const runMongo = async (nodeMongoFilePath, edgesMongoFilePath) => {
-  uploadUsersMongo(mongoClient, nodeMongoFilePath);
-  uploadEdgesUsersMongo(mongoClient, edgesMongoFilePath)
+const runMongo = async (nodeFilePath, edgesFilePath) => {
+  uploadUsersMongo(mongoClient, nodeFilePath);
+  uploadEdgesUsersMongo(mongoClient, edgesFilePath)
     .then(() => {
       console.log("Mongo :: Graph Data Loaded successfully");
       mongoClient.close();
@@ -66,11 +68,11 @@ const runMongo = async (nodeMongoFilePath, edgesMongoFilePath) => {
     .catch((err) => [console.log("Mongo :: Error Loading Graph Data: ", err)]);
 };
 
-async function uploadUsersMongo(mongoClient, nodeMongoFilePath) {
+async function uploadUsersMongo(mongoClient, nodeFilePath) {
   const database = mongoClient.db("users");
   const usersCollection = database.collection("usersData");
 
-  var records = await loadDataAsObjects(nodeMongoFilePath);
+  var records = await loadDataAsObjects(nodeFilePath);
   var user = {};
   for (const record of records) {
     const [name, age, username] = record;
@@ -85,17 +87,13 @@ async function uploadUsersMongo(mongoClient, nodeMongoFilePath) {
     const p = await usersCollection.insertOne(user);
     //console.log([name, age, username]);
   }
-
-  // let nodeMongoFilePath =
-  //   "C:/Users/bogda/Desktop/Scalable Data Management Systems/Project/scalableAssignment/src/datasets/fake_users2.csv";
-  // let objectData = loadDataAsObjects(nodeMongoFilePath);
 }
 
-async function uploadEdgesUsersMongo(mongoClient, edgesMongoFilePath) {
+async function uploadEdgesUsersMongo(mongoClient, edgesFilePath) {
   const database = mongoClient.db("users");
   const usersEdgesCollection = database.collection("usersEdgesData");
 
-  var records = await loadDataAsObjects(edgesMongoFilePath);
+  var records = await loadDataAsObjects(edgesFilePath);
   var edge = {};
   for (const record of records) {
     const [edgePredicate, sourceNode, destinationNode] = record;
@@ -111,15 +109,12 @@ async function uploadEdgesUsersMongo(mongoClient, edgesMongoFilePath) {
   }
 }
 
-let nodeMongoFilePath =
-  "C:/Users/bogda/Desktop/Scalable Data Management Systems/Project/scalableAssignment/src/datasets/fake_users2.csv";
-let edgesMongoFilePath =
-  "C:/Users/bogda/Desktop/Scalable Data Management Systems/Project/scalableAssignment/src/datasets/fake_social_media2.csv";
+let nodeFilePath = path.join(__dirname, 'datasets/fake_users2.csv');
+let edgesFilePath =  path.join(__dirname, 'datasets/fake_social_media2.csv');
 // pingMongo().catch(console.dir);
 
-//uploadUsersMongo(nodeMongoFilePath).catch(console.dir);
-//uploadEdgesUsersMongo(edgesMongoFilePath).catch(console.dir);
-runMongo(nodeMongoFilePath, edgesMongoFilePath);
+
+// runMongo(nodeFilePath, edgesFilePath);
 
 // LOAD ARANGO GRAPH
 
@@ -153,12 +148,12 @@ async function arangoLoadNodes(client, filePath) {
         UPSERT {
           _key: ${username},
           name: ${name},
-          age: ${age}
+          age: ${Number(age)}
         }
         INSERT {
           _key: ${username},
           name: ${name},
-          age: ${age}
+          age: ${Number(age)}
         }
         UPDATE {}
         IN "fakeSocialMediaWithAge"
@@ -224,13 +219,61 @@ const buildArangoGraph = async (nodeFilePath, edgesFilePath) => {
   ])
 
   console.log( "Node Insert Time: ", nodeInsertEndTime - startTime)
-  console.log( "Node Insert Time: ", edgeInsertEndTime - nodeInsertEndTime)
-  console.log( "Node Insert Time: ", edgeInsertEndTime - startTime)
+  // console.log( "Node Insert Time: ", edgeInsertEndTime - nodeInsertEndTime)
+  // console.log( "Node Insert Time: ", edgeInsertEndTime - startTime)
 }
 
-nodeFilePath =
-  "/home/w3ndo/Desktop/Course Work/Scalable Data Management Systems/project/src/datasets/fake_users2.csv";
-edgeDefinitionFilePath =
-  "/home/w3ndo/Desktop/Course Work/Scalable Data Management Systems/project/src/datasets/fake_social_media2.csv";
+// buildArangoGraph(nodeFilePath, edgesFilePath);
 
-//buildArangoGraph(nodeFilePath, edgeDefinitionFilePath);
+const singleReadQuery = async (client, collectionName) => {
+  // the FOR loop in AQL expects an iterable like a collection or array. We thus first create a collection object
+  let collection = client.collection(collectionName) 
+  const singleReadQuery = await client.query(aql`
+    FOR doc IN ${collection}
+    FILTER doc.age > 18 && doc.age < 28
+    LIMIT 1000
+    RETURN doc
+  `);
+
+  const doc = await singleReadQuery.all()
+  console.log(doc)
+}
+
+const singleWriteQuery = async (client, collectionName) => {
+  // as an example, we would want to find the first ten GenZ users (below 24) and make them mildly famous.
+  let collection = client.collection(collectionName);
+  const getGenZUsersQuery = await client.query(aql`
+    FOR user IN ${collection}
+    FILTER user.age < 24
+    LIMIT 10
+    RETURN user
+  `)
+  const genZ = await getGenZUsersQuery.all()
+
+  let popularUser = await genZ[Math.floor(Math.random() * genZ.length)] 
+  // get a random user to make popular
+  
+  // This query makes every other genZ person follow our randomly selected popular user
+  const makeFamousQuery = await client.query(aql`
+    FOR genZ IN ${genZ}
+      UPSERT {
+        _from: genZ._id,
+        _to: ${popularUser._id},
+        edge: {
+          name: "follows"
+        }
+      } INSERT {
+        _from: genZ._id,
+        _to: ${popularUser._id},
+        edge: {
+          name: "follows"
+        }
+      } UPDATE {}
+      IN "fakeSocialMediaWithAgeEdges"
+  `)
+
+  await makeFamousQuery.next()
+}
+
+
+singleWriteQuery(arangoClient, "fakeSocialMediaWithAge")
